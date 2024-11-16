fe299aa62f2731b114b27227926a665e7e77aa48
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-11-16 16:14:38 +0900

    Add: createBrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
