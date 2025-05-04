072dfe75d74b6c1fb2eeec2256953f730724fe7e
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-05-04 15:56:52 +0900

    Add: Disable CSS animation

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,6 +3,15 @@ import { expect, test } from "@playwright/test";
 test("notification test", async ({ page }) => {
   await page.goto("/");
 
+  await page.addStyleTag({
+    content: `
+      *, *::before, *::after {
+        transition: none !important;
+        animation: none !important;
+      }
+    `,
+  });
+
   await page.getByRole("button", { name: "Show notification" }).click();
   await expect(page.getByText("Default notification")).toBeVisible();
 
